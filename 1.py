f3abb15fd39cdfa5303f43b6a0424c987da3ea7d
carolinegbowski <caroline.gbowski@gmail.com>
2020-02-11 17:03:06 -0500

    1 mostly done, but needs some fixing

--- 1.py
+++ 1.py
@@ -14,9 +14,14 @@ return [0, 1].
 
 def twoSum(nums, target):
     index_list = []
-    for num in nums: 
-        for each_remaining_num in nums[num:]:
+    for num in nums:
+        index_1 = nums.index(num)
+        for each_remaining_num in nums[(index_1+1):]:
+            index_2 = nums.index(each_remaining_num)
             if num + each_remaining_num == target:
-                index_list.append(nums.index(num))
-                index_list.append(nums.index(each_remaining_num))
-    return index_list
+                index_list.append(index_1)
+                index_list.append(index_2)
+                return index_list
+
+# does not work when both nubmers are the same, but at different indicies
+# need to modify here
