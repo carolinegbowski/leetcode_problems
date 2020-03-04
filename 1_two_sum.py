0f2b75eabccdbdde0fbe23a3ab423f99bad35096
carolinegbowski <caroline.gbowski@gmail.com>
2020-03-04 11:32:48 -0500

    added comments twosum

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -15,20 +15,28 @@ return [0, 1].
 def twoSum(nums, target):
     index_list = []
     for num in nums: 
+        ## for each num in nums, find the remainder of target - num, see if it's even in the list
         remainder = target - num
         if remainder in nums:
+            # find index of first val, append it to output (so we know it's already been used)
             index1 = nums.index(num)
             index_list.append(index1)
             index2 = nums.index(remainder)
+            #make sure index2 isnt same as index1 (make sure we didnt already use that element)
             if index2 not in index_list:
+                #if not, we're done
                 index_list.append(index2)
                 return index_list
             else:
+                #if so, we check how many occurrances there are of said remainder
                 occurrence = [i for i, n in enumerate(nums) if n == remainder]
+                #if multiple, take the second occurrance
                 if len(occurrence) > 1:
                     index2 = occurrence[1]
                     index_list.append(index2)
                     return index_list
+                #if only one, we've used the same element twice and need to start over
+                #We clear our index list and let our program check the next num
                 else: 
                     index_list = []
 
