c256ca15faf6dfce7bb63efe587b67122d130793
carolinegbowski <caroline.gbowski@gmail.com>
2020-03-04 16:48:13 -0500

    added few problems, added comments to problems

--- 20_valid_parentheses.py
+++ 20_valid_parentheses.py
@@ -31,15 +31,22 @@ Output: true
 
 def isValid(s):
     opens = ["(", "{", "["]
+    # create an empty stack
     saved_opens = []
+    # account for base cases
+    # if the string is empty, automatic true
     if s == "":
         return True
+    # if first character is not an open, automatic false 
     if s[0] not in opens:
         return False
+    #iterate through string
     for char in s:
         try:
+            # append opens to stack 
             if char in opens: 
                 saved_opens.append(char)
+            # if closed, pop off top of stack. If not matching, return False
             if char == ")":
                 popped = saved_opens.pop()
                 if popped != "(":
@@ -52,7 +59,9 @@ def isValid(s):
                 popped = saved_opens.pop()
                 if popped != "{":
                     return False
+        # if we get an index error while popping, not a match. return false
         except IndexError:
             return False
+    # if we pop everything off and list is empty, we have valid parenthesis
     if saved_opens == []:
         return True
